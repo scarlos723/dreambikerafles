fad993bae6ad1950088f12ec7b3aa13b104ccf53
scarlos723 <carlossde58@gmail.com>
2023-08-21 02:12:17 -0500

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -24,73 +24,76 @@ const Home = () => {
   return (
     <main className='custom-container '>
       {
-        showNumberList && <NumberList />
-      }
-      <SliderContainer >
-        <Swiper
-          effect={'coverflow'}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={'auto'}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true
-          }}
-          pagination={true}
-          modules={[EffectCoverflow, Pagination, Autoplay]}
-          autoplay={{ delay: 3000 }}
-          className={'mySwiper'}>
-          {LIST_IMAGES.map((item, index) => (
-            <SwiperSlide key={index}>
-              <Article image={item} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </SliderContainer>
-      <Selector>
-        <div className='grid gap-2'>
-          <h1 className='text-center'>
+        showNumberList
+          ? <NumberList />
+          : <>
+            <SliderContainer >
+              <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: true
+                }}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination, Autoplay]}
+                autoplay={{ delay: 3000 }}
+                className={'mySwiper'}>
+                {LIST_IMAGES.map((item, index) => (
+                  <SwiperSlide key={index}>
+                    <Article image={item} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </SliderContainer>
+            <Selector>
+              <div className='grid gap-2'>
+                <h1 className='text-center'>
             Valor de la rifa $20.000 COP
-          </h1>
-          <p>
+                </h1>
+                <p>
           Cilindraje, 890 cc ; Peso (con aceite y tanque de gasolina lleno), 189 Kg ; Potencia, 117.3 Hp a 10000 rpm ; Torque, 93.0 Nm a 7000 rpm.
-          </p>
-          <h2>
+                </p>
+                <h2>
           Yamaha MT 09 2022
-          </h2>
-        </div>
-        <div className='progress-container'>
-          <div className='flex justify-between'>
-            <h5>
+                </h2>
+              </div>
+              <div className='progress-container'>
+                <div className='flex justify-between'>
+                  <h5>
               Meta
-            </h5>
-            <h5 className='text-[#EED202]'>
+                  </h5>
+                  <h5 className='text-[#EED202]'>
             50%
-            </h5>
-          </div>
-          <figure className='progress-bar' >
-            <div className='progress' />
-          </figure>
-        </div>
-        <div>
-          <h2>
+                  </h5>
+                </div>
+                <figure className='progress-bar' >
+                  <div className='progress' />
+                </figure>
+              </div>
+              <div>
+                <h2>
             Una vez lleguemos la meta,
-          </h2>
-          <h4>
+                </h2>
+                <h4>
             La rifa juega con las ultimas cifras de la  <span>
             loteria de Medellin
-            </span>
-          </h4>
-        </div>
-        <button className='uppercase font-extrabold text-[#EED202]'
-          onClick={() => setShowNumberList(true)}
-        >
+                  </span>
+                </h4>
+              </div>
+              <button className='uppercase font-extrabold text-[#EED202]'
+                onClick={() => setShowNumberList(true)}
+              >
           Comprar Numero
-        </button>
-      </Selector>
+              </button>
+            </Selector>
+          </>
+      }
 
     </main>
   )
